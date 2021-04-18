6ba97f8b59795f9473ba0bde7532c18aebef32d1
Viren Patel <75879651+Viren1993-web@users.noreply.github.com>
2021-04-19 00:44:49 +1200

    Final
    Advance 2

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Certificate.jsx
@@ -0,0 +1,18 @@
+﻿/* Certificate section */
+import React from 'react';
+import Cookies from 'js-cookie';
+
+export default class Certificate extends React.Component {
+
+    constructor(props) {
+        super(props)
+        
+
+    };
+
+    render() {
+        return(
+            <div>Certificates Sections</div>
+        )
+    }
+}
